b4916a1c047f63c773fc16d3904fdcde10262993
ayan98542 <ayan98542@gmail.com>
2022-11-16 04:17:46 +0100

    Kscope bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022 The kscopeOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_chime.mk
+    $(LOCAL_DIR)/kscope_chime.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_chime-user \
-    lineage_chime-userdebug \
-    lineage_chime-eng
+    kscope_chime-user \
+    kscope_chime-userdebug \
+    kscope_chime-eng
